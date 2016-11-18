f288500911ebd36205a255c8c36798f729203c98
Nicholas Galasso <nick.galasso@animoto.com>
2016-11-17 22:30:44 -0500

    updates

--- AmexRewards.safariextension/clickStuff.js
+++ AmexRewards.safariextension/clickStuff.js
@@ -1,13 +1,19 @@
 function handleMessage(msgEvent) {
 
+	console.log("running");
+
     var messageName = msgEvent.name;
 
     if (messageName === "clickAddToCard") {
         
-		var items = document.querySelectorAll("a.ah-add-to-card-button");
-		
+		var items = document.querySelectorAll("button.ah-btn");
+		console.log(items.length);	
 		for (var i = 0; i < items.length; i++) {
-			items[i].click()
+         console.log('entered loop');
+			if (items[i].title == "Add to Card"){
+				console.log("added offer");
+				items[i].click();	
+			}
 		}
     }
 }
